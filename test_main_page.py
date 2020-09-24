64e4a75047c6a10680db1fce370b4c101c33eb8b
screamo1107 <screamo1107@gmail.com>
2020-09-24 20:11:49 +0200

    Refactoring using PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,8 @@
-import math
-import pytest
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
