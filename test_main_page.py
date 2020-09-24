71d3acf7c6395a5ab268427b9c0495d9b1d8c0d7
screamo1107 <screamo1107@gmail.com>
2020-09-24 20:31:08 +0200

    Refactoring: is_element_present check for BasePage adding

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,14 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.should_be_login_link()
