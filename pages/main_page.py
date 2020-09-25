4aa289fdacfed4b814c8c94b50c996e570f87af3
screamo1107 <screamo1107@gmail.com>
2020-09-25 11:18:25 +0200

    Updated test: check navigation from main to login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,9 @@ from .locators_m import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
