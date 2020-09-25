e5f735aaa4f48fe06a561eb71693bd7c9048f250
screamo1107 <screamo1107@gmail.com>
2020-09-25 14:18:13 +0200

    Product page related test, updated base_page and locators

--- pages/locators_m.py
+++ pages/locators_m.py
@@ -8,3 +8,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADDED_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
+    ITEM_PRICE = (By.CSS_SELECTOR, ".price_color")
+    TOTAL_AMOUNT_BASKET = (By.CSS_SELECTOR, ".basket-mini")
---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+from .pages.product_page import ProductPage
+import time
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_msg_added_to_basket()
+    page.should_be_total_price_of_basket()
