82b05b18015ebb126d7f0e44576272015638aeb7
screamo1107 <screamo1107@gmail.com>
2020-09-26 13:46:58 +0200

    Locators refactoring

--- pages/locators_m.py
+++ pages/locators_m.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    VIEW_BASKET_SUMMARY = (By.CSS_SELECTOR, ".btn-group a")
-    VIEW_BASKET_SUMMARY_INV = (By.CSS_SELECTOR, ".btn-group-gr")
+    VIEW_BASKET_SUMMARY_BUTTON = (By.CSS_SELECTOR, ".btn-group a")
+    VIEW_BASKET_SUMMARY_BUTTON_INV = (By.CSS_SELECTOR, ".btn-group-gr")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # duplicated in Base
+    pass
 
 
 class LoginPageLocators:
@@ -24,13 +24,13 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    SELECTED_ITEM = (By.CSS_SELECTOR, ".product_main h1")
     ADDED_ITEM_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
     ITEM_PRICE = (By.CSS_SELECTOR, ".price_color")
-    TOTAL_AMOUNT_BASKET = (By.CSS_SELECTOR, ".basket-mini")
+    SELECTED_ITEM = (By.CSS_SELECTOR, ".product_main h1")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+    TOTAL_AMOUNT_BASKET = (By.CSS_SELECTOR, ".basket-mini")
 
 
 class BasketPageLocators:
-    BASKET_ITEMS_ELEMENT = (By.CSS_SELECTOR, ".basket_summary")
     BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_ITEMS_ELEMENT = (By.CSS_SELECTOR, ".basket_summary")
